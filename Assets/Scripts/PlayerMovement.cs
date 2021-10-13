a36b773e57d4700fd9c23719e0d9b5058aeeb0b1
Ayush Kumar <ayushrocks456@gmail.com>
2021-10-13 20:16:14 +0530

    Character Controller Improved

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,49 +1,59 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Transform playerCamera = null;
+    [SerializeField]
+    private float mouseSensitivity;
+    [SerializeField]
+    private bool cursorLocked = true;
+    private float cameraPitch;
+    [SerializeField]
+    private float mouseSmoothTime = 0.03f;
+    private Vector2 currentMouseDelta = Vector2.zero;
+    private Vector2 currentMouseDeltaVelocity = Vector2.zero;
 
+    private CharacterController playerController;
     [SerializeField]
-    private float _movementSpeed;
+    private float movementSpeed;
     [SerializeField]
-    private float sensitivity;
-    private Camera _mainCamera;
-    [SerializeField]
-    private GameObject _gunObject;
+    private float smoothTime = 0.3f;
+    private Vector2 currentDir = Vector2.zero;
+    private Vector2 currentDirVelocity = Vector2.zero;
 
+    private void Awake()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
     private void Start()
     {
-        _mainCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.left * _movementSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.right * _movementSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.back * _movementSpeed * Time.deltaTime);
-        }
-        float xMouseValue = Input.GetAxis("Mouse X");
-        float yMouseValue = Input.GetAxis("Mouse Y");
-        if (xMouseValue != 0 || yMouseValue != 0)
-        {
-            //Debug.Log(xMouseValue);
-            //Debug.Log(" " + yMouseValue);
-            //if(transform.rotation.)
-            //transform.Rotate(new Vector3(0.0f, xMouseValue * sensitivity * Time.deltaTime, yMouseValue * sensitivity * Time.deltaTime));
-            _mainCamera.transform.Rotate(new Vector3(0.0f, xMouseValue * sensitivity * Time.deltaTime, yMouseValue * sensitivity * Time.deltaTime));
-            _gunObject.transform.Rotate(new Vector3(0.0f, xMouseValue * sensitivity * Time.deltaTime, yMouseValue * sensitivity * Time.deltaTime));
-        }
+        MouseUpdate();
+        Movement();
+    }
 
+    private void MouseUpdate()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+        cameraPitch -= currentMouseDelta.y;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+        transform.Rotate(currentMouseDelta.x * mouseSensitivity * Vector3.up);
+    }
+
+    private void Movement()
+    {
+        Vector2 targetDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDirection.Normalize();
+        currentDir = Vector2.SmoothDamp(currentDir, targetDirection, ref currentDirVelocity, smoothTime);
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * movementSpeed;
+        playerController.Move(velocity * Time.deltaTime);
     }
 }
